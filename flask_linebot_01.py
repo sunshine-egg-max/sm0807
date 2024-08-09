cae66885862c142e88bcb52550dc71ed4d2e70b1
sunshine-egg-max <lyue13519@gmail.com>
2024-08-09 10:43:22 +0800

    [refactor]flask app, handle secret key & tokens in other

--- flask_linebot_01.py
+++ flask_linebot_01.py
@@ -23,34 +23,20 @@ from linebot.v3.webhooks import (
     TextMessageContent # 因特定事件傳訊息 例:退群訊息 # 使用者傳過來的資料格式
 )
 
-
-import os, sys
-
-# 1. 先到Line Developer Console, 把 Channel Secret & Channel Access Token 複製起來
-# 2. 把這兩個密文,存到環境變數內: 工具列搜尋<環境變數>,新增兩個環境變數,並且把值貼上去
-# 3. 按下確定儲存,要記得你的變數名稱,這些資訊只會存在你當前使用的電腦裡
-# 4. 透過以下程式碼,取得環境變數儲存的對應數值
-
-channel_secret = os.getenv('LINEBOT_SECRET_KEY', None)
-channel_access_token = os.getenv('LINEBOT_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINEBOT_SECRET_KEY as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
+from handle_keys import get_secret_and_token
+keys = get_secret_and_token()
 app = Flask(__name__) # Flask
-configuration = Configuration(access_token=channel_access_token)
-handler = WebhookHandler(channel_secret)
+handler = WebhookHandler(keys['channel_secret_key'])
+configuration = Configuration(access_token=keys['channel_access_token'])
+
 
 @app.route('/')
 def say_hello_world(username=''):
     return render_template('hello.html',name=username)
-# 設計一個 #callback 的路由, 提供給 Line 官方後台去呼叫
-# 也就是所謂的呼叫 Webhook Server
-# 因為官方會把使用者傳輸的訊息轉傳給 Webhook Server
-# 所以會使用 RESTful API 的 POST 方法
+    # 設計一個 #callback 的路由, 提供給 Line 官方後台去呼叫
+    # 也就是所謂的呼叫 Webhook Server
+    # 因為官方會把使用者傳輸的訊息轉傳給 Webhook Server
+    # 所以會使用 RESTful API 的 POST 方法
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -69,12 +55,12 @@ def callback():
 
     return 'OK'
 
-
-# 根據不同的使用者事件(event),去用不同的方式回應
-# eg. MessageEvent 代表使用者單純傳訊息的事件
-# TextMessageContent 代表使用者傳輸的訊息內容是文字
-# 符合兩個條件的事件,會被handle_message 所處理
 @handler.add(MessageEvent, message=TextMessageContent)
+
+    # 根據不同的使用者事件(event),去用不同的方式回應
+    # eg. MessageEvent 代表使用者單純傳訊息的事件
+    # TextMessageContent 代表使用者傳輸的訊息內容是文字
+    # 符合兩個條件的事件,會被handle_message 所處理
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
