a86281174bbfc833aa668740c8003adfab53467e
sunshine-egg-max <lyue13519@gmail.com>
2024-08-09 11:03:11 +0800

    [check]

--- flask_linebot_01.py
+++ flask_linebot_01.py
@@ -1,46 +1,47 @@
 from flask import (
-    Flask,
-    request,
-    abort,
+    Flask, 
+    request, 
+    abort, 
     render_template
-    )
-
+)
 from linebot.v3 import (
     WebhookHandler
 )
 from linebot.v3.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.messaging import ( # 傳訊息
+from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage # 傳輸回Line官方後台的資料格式
+    TextMessage  # 傳輸回Line官方後台的資料格式
 )
 from linebot.v3.webhooks import (
-    MessageEvent, # 單純傳訊息 # 傳輸過來方法
-    TextMessageContent # 因特定事件傳訊息 例:退群訊息 # 使用者傳過來的資料格式
+    MessageEvent, # 傳輸過來的方法
+    TextMessageContent # 使用者傳過來的資料格式
 )
-
 from handle_keys import get_secret_and_token
+
+app = Flask(__name__)
 keys = get_secret_and_token()
-app = Flask(__name__) # Flask
-handler = WebhookHandler(keys['channel_secret_key'])
-configuration = Configuration(access_token=keys['channel_access_token'])
+handler = WebhookHandler(keys['LINEBOT_SECRET_KEY'])
+configuration = Configuration(access_token=keys['LINEBOT_ACCESS_TOKEN'])
 
+@app.route("/")
+def say_hello_world(username=""):
+    # 測試用，確定webhook server 有連通
+    return render_template("hello.html", name=username)
 
-@app.route('/')
-def say_hello_world(username=''):
-    return render_template('hello.html',name=username)
-    # 設計一個 #callback 的路由, 提供給 Line 官方後台去呼叫
-    # 也就是所謂的呼叫 Webhook Server
-    # 因為官方會把使用者傳輸的訊息轉傳給 Webhook Server
-    # 所以會使用 RESTful API 的 POST 方法
 @app.route("/callback", methods=['POST'])
 def callback():
+    # 設計一個 #callback 的路由，提供給Line官方後台去呼叫
+    # 也就所謂的呼叫Webhook Server
+    # 因為官方會把使用者傳輸的訊息轉傳給Webhook Server
+    # 所以會使用 RESTful API 的 POST 方法
+
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature'] # 標頭檔
+    signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -56,18 +57,19 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessageContent)
-
-    # 根據不同的使用者事件(event),去用不同的方式回應
+def handle_message(event):
+    # 根據不同的使用者事件(event)，用不同的方式回應
     # eg. MessageEvent 代表使用者單純傳訊息的事件
     # TextMessageContent 代表使用者傳輸的訊息內容是文字
-    # 符合兩個條件的事件,會被handle_message 所處理
-def handle_message(event):
+    # 符合兩個條件的事件，會被handle_message 所處理
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)] # 改詞
+                messages=[
+                    TextMessage(text=event.message.text)
+                ]
             )
         )
 
